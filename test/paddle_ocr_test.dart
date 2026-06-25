f38f5295756591772533812c7d9ef0b93f039a88
ubuntu2610 <1413301@163.com>
2026-06-25 14:16:53 +0800

    Fix test import path to match pp_ocr package name

--- test/paddle_ocr_test.dart
+++ test/paddle_ocr_test.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pp_ocr/paddle_ocr.dart';
+import 'package:pp_ocr/pp_ocr.dart';
 import 'package:pp_ocr/paddle_ocr_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
